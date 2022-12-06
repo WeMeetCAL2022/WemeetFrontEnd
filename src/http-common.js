3a4f1dd404423ea5d671179c0a55544b63000843
Julien MICHOT <70885964+jmichot@users.noreply.github.com>
2022-12-06 09:34:20 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: 'https://arcane-castle-91734.herokuapp.com/wemeet/',
-    baseURL: 'http://localhost:8080/wemeet/',
+    baseURL: 'https://shielded-mountain-29706.herokuapp.com/wemeet/',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Method': '*',
